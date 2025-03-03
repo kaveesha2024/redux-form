3197942baa5cf5550ceefc391c43633186fc4a74
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-03-03 16:11:39 +0530

    persist store configed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {Provider} from "react-redux";
-import store from './store'
-
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-  // </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { persist, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persist}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>,
+);
--- src/store.js
+++ src/store.js
@@ -1,8 +1,20 @@
 import {configureStore} from "@reduxjs/toolkit";
 import formReducer from "./utility/formSlice.js";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export default configureStore({
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+export const persistedReducer = persistReducer(persistConfig, formReducer)
+
+
+export const store = configureStore({
     reducer: {
-        form: formReducer,
+        form: persistedReducer,
     },
 });
+
+export const persist = persistStore(store)
